159c84a62f6e53af4c61af9ec32fb290d2826bc9
simonearata <simone.arata1@gmail.com>
2021-04-12 09:39:40 +0200

    second

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ var numeri = []
 for (var i = 0; i <= 100; i++){
   numeri = i
   
-  if(numeri%3 && numeri%5){
-    console.log(numeri);
+  if(numeri%3 == 0 && numeri%5 == 0){
+    console.log("FizzBuzz");
   }else if(numeri%3 == 0){
     console.log("Fizz")
   }else if(numeri%5 == 0){
     console.log("Buzz")
+  }else{
+    console.log(numeri)
   }
 }
 
